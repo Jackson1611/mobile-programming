6f6446f84cb66427bb50f8450f79824667a2292f
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-01-20 22:40:44 +0200

    my-app

--- my-app/App.js
+++ my-app/App.js
@@ -1,41 +1,26 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput, Switch } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+
 
 export default function App() {
-  const [message, setMessage] = React.useState('');
-  const [isDarkMode, setIsDarkMode] = React.useState(false);
 
-  const buttonPressed = () => {
-    Alert.alert('you typed ' + message)
-  };
+const [message, setMessage] = React.useState('');
+
+const buttonPressed = () => {
+Alert.alert('you typed ' + message)
+
+};
+
 
   return (
-    <View style={[styles.container, {backgroundColor: isDarkMode ? 'black' : 'white'}]}>
+    <View style={styles.container}>
       <TextInput 
-        style={[styles.inputStyle, {backgroundColor: isDarkMode ? 'gray' : 'white'}]}
-        value={message} 
-        onChangeText={(text) => setMessage(text)}
-      />
-      <Text style={[styles.textStyle, {color: isDarkMode ? 'white' : 'black'}]}>
-        Hello World
-      </Text>
-      <View style={styles.buttonContainer}>
-        <Button 
-          onPress={buttonPressed}
-          title="Click the button"
-          color={isDarkMode ? 'white' : 'black'}
-        />
-      </View>
-      <View style={styles.switchContainer}>
-        <Text style={[styles.textStyle, {color: isDarkMode ? 'white' : 'black'}]}>
-          Dark Mode
-        </Text>
-        <Switch 
-          value={isDarkMode} 
-          onValueChange={() => setIsDarkMode(!isDarkMode)} 
-        />
-      </View>
+        style = {styles.inputStyle}
+        value = {message} 
+        onChangeText = {(text) => setMessage(text)}/>
+      <Text>Hello World</Text>
+      <Button onPress={buttonPressed} title ="Click the button" />
       <StatusBar style="auto" />
     </View>
   );
@@ -44,28 +29,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   inputStyle:{
-    width: 200,
-    borderColor: "blue",
-    borderWidth: 1,
-    marginBottom: 5,
-    padding: 5,
-    borderRadius: 5,
-  },
-  textStyle: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  width: 200,
+  borderColor: "blue",
+  borderWidth: 1,
+  marginBottom: 5,
+  padding: 5
+
+
+}
+
 });
