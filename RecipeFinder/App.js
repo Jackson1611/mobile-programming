c304737eadb56e02ecf779f8e771c1d266d836a7
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-02-05 23:39:30 +0200

    Networking first assignment completed

---
+++ RecipeFinder/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image, StyleSheet, TextInput, Button } from 'react-native';
+
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleFind = async () => {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`
+    );
+    const data = await response.json();
+    setRecipes(data.meals);
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={recipes}
+        keyExtractor={(item) => item.idMeal}
+        renderItem={({ item }) => (
+          <View style={styles.recipeContainer}>
+            <Image source={{ uri: item.strMealThumb }} style={styles.thumbnail} />
+            <Text style={styles.title}>{item.strMeal}</Text>
+          </View>
+        )}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+          placeholder="Enter ingredient"
+        />
+        <Button title="Find" onPress={handleFind} style={styles.button} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  inputContainer: {
+    width: '80%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 70,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginTop: 10,
+  },
+  recipeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 10,
+  },
+  thumbnail: {
+    width: 80,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 15,
+  },
+  button: {
+    padding: 20,
+    fontSize: 20,
+  },
+
+  
+});
+
+export default App;
